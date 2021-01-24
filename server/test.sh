f086f9dd25554a851f37431a4a5fcf95a2549323
hiroyuki.obinata <hiroyuki.obinata@gmail.com>
2021-01-24 02:46:43 +0000

    increase memory size

--- server/test.sh
+++ server/test.sh
@@ -21,4 +21,4 @@ apikey=${IV}$(echo -n ${cipher} | xxd -p -c 128)
 image=$(base64 -w0 ./201905101119-77957.png)
 
 # success
-echo {\"image\": \"${image}\", \"tsumo\": \"true\", \"apikey\": \"${apikey}\", \"version\": \"1\" } | curl -X POST -H "Content-Type: application/json" -d @-  ${endpoint}
+time echo {\"image\": \"${image}\", \"tsumo\": \"true\", \"apikey\": \"${apikey}\", \"version\": \"1\" } | curl -X POST -H "Content-Type: application/json" -d @-  ${endpoint}
